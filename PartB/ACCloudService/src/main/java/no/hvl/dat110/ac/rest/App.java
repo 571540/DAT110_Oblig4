4349c7181cba1137c6a76f67f858513928173771
Anders Kvamsøe <31222903+Kvamsoe@users.noreply.github.com>
2019-04-29 15:39:24 +0200

    .

--- PartB/ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
+++ PartB/ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
@@ -8,16 +8,19 @@ import static spark.Spark.post;
 import static spark.Spark.delete;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import okhttp3.RequestBody;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	
+
 	static AccessLog accesslog = null;
 	static AccessCode accesscode = null;
-	
+
 	public static void main(String[] args) {
 
 		if (args.length > 0) {
@@ -27,24 +30,66 @@ public class App {
 		}
 
 		// objects for data stored in the service
-		
+
 		accesslog = new AccessLog();
-		accesscode  = new AccessCode();
-		
+		accesscode = new AccessCode();
+
 		after((req, res) -> {
-  		  res.type("application/json");
-  		});
-		
+			res.type("application/json");
+		});
+
 		// for basic testing purposes
 		get("/accessdevice/hello", (req, res) -> {
-			
-		 	Gson gson = new Gson();
-		 	
-		 	return gson.toJson("IoT Access Control Device");
+
+			Gson gson = new Gson();
+
+			return gson.toJson("IoT Access Control Device");
 		});
-		
+
 		// TODO: implement the routes required for the access control service
-		
-    }
-    
+
+		post("/accessdevice/log", (req, res) -> {
+			Gson gson = new Gson();
+			JsonObject object = gson.fromJson(req.body(), JsonObject.class);
+			String message = object.get("message").getAsString();
+
+			int id = accesslog.add(message);
+
+			return gson.toJson(accesslog.get(id));
+		});
+		get("/accessdevice/log/:id", (req, res) -> {
+			int id = -1;
+			try {
+				id = Integer.parseInt(req.params(":id"));
+			} catch (Exception e) {
+				System.out.println("Invalid id");
+			}
+
+			Gson gson = new Gson();
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		delete("/accessdevice/log", (req, res) -> {
+			accesslog.clear();
+			return accesslog.toJson();
+		});
+
+		get("accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			return gson.toJson(accesscode);
+		});
+
+		put("accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			AccessCode accessCode = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(accessCode.getAccesscode());
+
+			return req.body();
+		});
+
+	}
+
 }
